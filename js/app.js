9b538aa9a4877664cd067f3706618206a7e8c424
Jose Pereira <jafcpereira@gmail.com>
2016-02-24 20:24:20 +0000

    Make google maps load asynchronously

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 'use strict';
 
-// Google Maps API and Places library functions to start the map
 var map;
 var service;
 var infowindow;
 var marker;
+
 
 
 // Hard-Coded 9 locations
@@ -50,36 +50,40 @@ var locations = [{
 
 
 
-$(function() {
+var googleSuccess = function () {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: 40.209658,
+            lng: -8.419721
+        },
+        zoom: 18,
+        disableDefaultUI: true
+    });
+
     function viewAppModel() {
         var self = this;
-        var coimbra = new google.maps.LatLng(40.209658,-8.419721);
-        
-        // Function to initialize the map API
-        self.map = new google.maps.Map(document.getElementById('map'), {
-                center: coimbra,
-                zoom: 18,
-                disableDefaultUI: true
-            });
-        
+
         function Location(data) {
             this.name = data.name;
             this.lat = data.lat;
             this.long = data.long;
         }
-        
+
         // Empty array created to hold all places that will be added with the forEach loop through the var locations above.
-        
+
         self.allLocations = [];
-        locations.forEach(function(location) {
+        locations.forEach(function (location) {
             self.allLocations.push(new Location(location));
         });
-        
+
         // Adding markers to map. TODO: Integrate API
-        self.allLocations.forEach(function(location) {
+        self.allLocations.forEach(function (location) {
             location.marker = new google.maps.Marker({
-                map: self.map,
-                position: {lat: location.lat, lng: location.long},
+                map: map,
+                position: {
+                    lat: location.lat,
+                    lng: location.long
+                },
                 animation: google.maps.Animation.DROP,
                 name: location.name
             });
@@ -90,51 +94,50 @@ $(function() {
                     location.marker.setAnimation(google.maps.Animation.BOUNCE);
                 }
                 // Adding a timeout function to avoid the marker bouncing forever (only 2 secs)
-                setTimeout(function() {
+                setTimeout(function () {
                     location.marker.setAnimation(null);
-                },2000);
+                }, 2000);
             });
         });
-        
+
         // Implementing list view. Code adapted from http://opensoul.org/2011/06/23/live-search-with-knockoutjs/
-        
+
         // Ko array that will have the filtered locations on the map. Default state when starting the app is with all locations visible
         self.filteredLocations = ko.observableArray();
-        
-        self.allLocations.forEach(function(location) {
+
+        self.allLocations.forEach(function (location) {
             self.filteredLocations.push(location);
         });
-        
+
         // Implementing the search and filter functionality.
-        
+
         // Getting user input in the search bar
-        
+
         self.userSearch = ko.observable('');
-        
+
         // Using user input text to filter the observable Array - function
-        
-        self.filterLocations = function() {
+
+        self.filterLocations = function () {
             var textInput = self.userSearch().toLowerCase();
-            
+
             // Remove everything from the filteredArray
-            
+
             self.filteredLocations.removeAll();
-            
+
             // Use the user input to filter locations by name
-            
-            self.allLocations.forEach(function(location) {
+
+            self.allLocations.forEach(function (location) {
                 location.marker.setMap(null);
-                
+
                 if (location.name.toLowerCase().indexOf(textInput) > -1) {
                     self.filteredLocations.push(location);
                 }
             });
-            
-            self.filteredLocations().forEach(function(location) {
-                location.marker.setMap(self.map);
+
+            self.filteredLocations().forEach(function (location) {
+                location.marker.setMap(map);
             });
         };
     }
     ko.applyBindings(new viewAppModel());
-});
-
+}
