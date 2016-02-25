24b9d42c96ad26e32b248531c1efd72583341dba
Jose Pereira <jafcpereira@gmail.com>
2016-02-25 15:03:45 +0000

    Change locations, try to implement FourSquares API

--- js/app.js
+++ js/app.js
@@ -5,48 +5,62 @@ var service;
 var infowindow;
 var marker;
 
-$('.search').submit(function(event) {
-    event.preventDefault();
-});
+// Data for the FourSquare API
+var CLIENT_ID = 'DYEYVAX33FSMO55QEUBSU3OWBPHV44LPUIOOKEOTBD4XWVIM';
+var CLIENT_SECRET = 'ZI11DWJ0KASGKIWOF4WT2TVYZEVVT15QUX4M2VNYOEITINMA';
 
 // Hard-Coded 9 locations
 
 var locations = [{
     name: 'Cartola',
     lat: 40.209705,
-    long: -8.420236
+    long: -8.420236,
+    id: '4bf7100a5ec320a1325286d3'
 }, {
     name: 'Jardim da Sereia',
     lat: 40.209861,
-    long: -8.419238
+    long: -8.419238,
+    id: '4c951307f7cfa1cdef04ba15'
 }, {
-    name: 'Tapas',
-    lat: 40.208775,
-    long: -8.419592
+    name: 'Psicológico na Praça',
+    lat: 40.209100,
+    long: -8.420177,
+    id: '4e7dc84300397c8dcfe88b65'
+    
 }, {
     name: 'NS Hostel & Suites',
     lat: 40.210066,
     long: -8.418814
 }, {
     name: 'Noites Longas',
-    lat: 40.208267,
-    long: -8.418208
+    lat: 40.208259,
+    long: -8.418168,
+    id: '4c771f99947ca1cd0dab4637'
 }, {
     name: 'Aqui Base Tango',
-    lat: 40.208135,
-    long: -8.418752
+    lat: 40.208151,
+    long: -8.418747,
+    id: '5001ba9ae4b06407f6d277c7'
 }, {
-    name: 'Municipal Police',
-    lat: 40.209561,
-    long: -8.422350
+    name: 'Cantinas Amarelas',
+    lat: 40.208533,
+    long: -8.420981,
+    id: '5117a303e4b0631444e2b79a'
 }, {
-    name: 'Faculty of Psychology',
-    lat: 40.209561,
-    long: -8.422350
+    name: 'NB Club Coimbra',
+    lat: 40.208010,
+    long: -8.420422,
+    id: '4cfd54d2fabc2d439c33ded2'
 }, {
-    name: 'Faculty of Architecture',
-    lat: 40.209513,
-    long: -8.423293
+    name: 'Café Académico',
+    lat: 40.209044,
+    long: -8.419471,
+    id: '4c655288f07e2d7f458b9150'
+}, {
+    name: 'Café Tropical',
+    lat: 40.209054,
+    long: -8.419566,
+    id: '4bae6b89f964a52012b03be3'
 }];
 
 // In case of no connection to the google Maps API this function runs
@@ -75,7 +89,8 @@ function googleSuccess() {
             this.long = data.long;
             this.openWindow = function() {
                 this.marker.infoWindow.open(map, this.marker);
-            }
+            };
+            this.id = data.id;
         }
 
         // Empty array created to hold all places that will be added with the forEach loop through the var locations above.
@@ -109,6 +124,7 @@ function googleSuccess() {
             });
             
             location.marker.addListener('click', function toggleBounce() {
+                getFourSquare();
                 location.marker.infoWindow.open(map);
                 if (location.marker.getAnimation() !== null) {
                     location.marker.setAnimation(null);
@@ -121,14 +137,19 @@ function googleSuccess() {
                 }, 2000);
             });
             
-            // Function to call the Yelp API. Code adapted from https://discussions.udacity.com/t/how-to-make-ajax-request-to-yelp-api/13699/5
+            // Function to call the FourSquares API. Code adapted from https://discussions.udacity.com/t/inconsistent-results-from-foursquare/39625/7
             
-            // Helper function needed for Yelp API
-            function nonce_generate() {
-                return(Math.floor(Math.random() * 1e12).toString());
-            }
+            function getFourSquare() {
+                $.ajax({
+                    url: 'https://api.foursquare.com/v2/venues/' + location.id + '?client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET + '&v=20140806',
+                    sucess: function(data) {
+                        console.dir(data);
+                    }
+                    
+                }).fail(alert('Foursquare data is unavailable. Please try refreshing later.'))
+            };
             
-            
+           
             
         });
         
